2cae23a3b404892e6490d36f87aebf52c5b0017e
Sriniketh <kodesaisriniketh@gmail.com>
2026-06-01 02:09:01 +0530

    clerk middlewares

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +30,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
-}
+     <ClerkProvider>
+      <html lang="en">
+        <body className={innerClassName}>{children}</body>
+       </html>
+      </ClerkProvider>
+    );
+  }   
---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+    "/signin",
+    "/signip",
+    "/",
+    "/home"
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos"
+])
+
+export default clerkMiddleware((auth,req) => {
+    const {userId} = auth();
+    const currentUrl = new URL(req.url);
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+    // Always run for Clerk-specific frontend API routes
+    "/__clerk/(.*)",
+  ],
+};
